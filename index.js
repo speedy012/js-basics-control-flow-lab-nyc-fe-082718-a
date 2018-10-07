4812dcdb9c2a1013578ea73ef8ef014f4573765d
Herman <hrivas5726@aol.com>
2018-10-07 12:24:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet (feet) {
+if (feet <= 400) {
+  return 'This one is on me!';
+ } else if (feet <= 2500) {
+   return 'I will gladly take your thirty bucks.'
+ } else {
+   return 'No can do.';
+}
+}
+
+function ternaryCheckCity(city) {
+  // if (city === 'NYC')
+  // return 'Ok, sounds good.'
+  //} return "No, go."
+  return ((city === 'NYC') ? 'Ok, sounds good.' : "No go.");
+  //}
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.';  
+    default:
+      return 'Bye.';
+  }
+} 
+
+
+
+
