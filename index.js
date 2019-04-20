6bb0e56012d3183681a4ba40759aeed4214ca5c0
Herman Rivas <hrivas5726@aol.com>
2019-04-20 15:59:19 -0400

    Done.

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is on me!'
   }
-  else if (feet > 2000 && feet < 2500){
-    return "I will gladly take your thirty bucks.";
-  }
-  else {
-    return "No can do."
-  }
-}
+   else if (feet <= 2500 ){
+     return 'I will gladly take your thirty bucks.'
+   }
+   else if (feet > 2500) {
+     return 'No can do.'
+   }
+
 
 function ternaryCheckCity(city){
  if (city === "NYC"){
