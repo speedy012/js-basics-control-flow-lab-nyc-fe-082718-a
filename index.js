62fa89211d2fcbab59df7ff9d7db33bcf4e3da9a
Herman Rivas <hrivas5726@aol.com>
2019-04-20 15:57:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  }
+  else if (feet > 2000 && feet < 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city){
+ if (city === "NYC"){
+   return "Ok, sounds good."
+  }
+  else if (city !== "NYC"){
+   return "No go."
+ }
+}
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return "Thank you so much."
+  }
+  else if (tip === "not as generous"){
+    return "Thank you."
+  }
+  else if (tip === "thanks for everything"){
+    return "Bye."
+  }
+}
